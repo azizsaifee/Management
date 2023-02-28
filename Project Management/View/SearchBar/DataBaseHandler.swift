32339bcad37d31d7c6d85e1032cc5ce68b78c72e
“Mayank <azizsaifee123@gmail.com>
2023-02-28 16:52:16 +0530

    Ankit changes on search bar

--- Project Management/View/SearchBar/DataBaseHandler.swift
+++ Project Management/View/SearchBar/DataBaseHandler.swift
@@ -11,22 +11,40 @@ import UIKit
 
 class DataBaseHandler {
     
+//
+//    func fetchSearchedData(questionNo:String) -> [Questions]
+//    {
+//
+//        var sdata = [Questions]()
+//let predicate = NSPredicate(format: "questionNo CONTAINS[cd] %@ OR question CONTAINS[cd] %@ OR option1 CONTAINS[cd] %@ OR option2 CONTAINS[cd] %@ OR option3 CONTAINS[cd] %@ OR option4 CONTAINS[cd] %@" , questionNo,questionNo,questionNo,questionNo,questionNo,questionNo)
+//        let context = PersistentStorage.shared.context
+//        let request:NSFetchRequest = Questions.fetchRequest()
+//        request.predicate = predicate
+//        do{
+//            sdata = try(context.fetch(request))
+//        }
+//        catch{
+//            print("error")
+//        }
+//        return sdata
+//    }
     
-    func fetchSearchedData(questionNo:String) -> [Questions]
-    {
-        
+    func fetchSearchedData(searchText: String) -> [Questions] {
         var sdata = [Questions]()
-        let predicate = NSPredicate(format: "questionNo==%@", questionNo)
         let context = PersistentStorage.shared.context
-        let request:NSFetchRequest = Questions.fetchRequest()
+        let request: NSFetchRequest<Questions> = Questions.fetchRequest()
+        let predicate = NSCompoundPredicate(orPredicateWithSubpredicates: [
+            NSPredicate(format: "questionNo CONTAINS[c] %@", searchText),
+            NSPredicate(format: "AppCoreData.property CONTAINS[c] %@", searchText),
+            //NSPredicate(format: "UserDetail.property CONTAINS[c] %@", searchText)
+        ])
         request.predicate = predicate
-        do{
-            sdata = try(context.fetch(request))
-        }
-        catch{
-            print("error")
+        do {
+            sdata = try context.fetch(request)
+        } catch {
+            print("Error fetching searched data: \(error.localizedDescription)")
         }
         return sdata
     }
-    
+
 }
--- Project Management/View/SearchBar/SearchViewController.swift
+++ Project Management/View/SearchBar/SearchViewController.swift
@@ -13,19 +13,13 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var tableview: UITableView!
     
-    let data = ["h1","h2","a","a1","b","b1","c"]
-    var filterdata: [String]!
     
     var sdata = [Questions]()
-   // var sdata2 = [AppCoreData]()
-   // static var objRepositorys = AppDataRepositorys()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
        navigationController?.navigationBar.isHidden = true
-        filterdata = data
         let dh = DataBaseHandler()
-        
         tableview.delegate = self
         tableview.dataSource = self
         
@@ -35,50 +29,30 @@ class SearchViewController: UIViewController {
 }
 extension SearchViewController :UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       // return filterdata.count
         return sdata.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableview.dequeueReusableCell(withIdentifier: "SearchTableViewCell", for: indexPath) as! SearchTableViewCell
-       // cell.textLabel?.text = filterdata[indexPath.row]
         cell.questionlabel.text = sdata[indexPath.row].question
         cell.option1label.text = sdata[indexPath.row].option1
         cell.option2label.text = sdata[indexPath.row].option2
         cell.option3label.text = sdata[indexPath.row].option3
         cell.option4label.text = sdata[indexPath.row].option4
-      //  cell.textLabel?.text = sdata2[indexPath.row].description
+     
         return cell
     }
-    
-    
 }
 
 extension SearchViewController :UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        filterdata = []
-//        if searchText == ""{
-//            filterdata = data
-//        }
-//        for word in data{
-//            if word.uppercased().contains(searchText.uppercased()){
-//                filterdata.append(word)
-//            }
-//        }
-//        self.tableview.reloadData()
+
         let dh = DataBaseHandler()
         if searchText != ""{
-            sdata = dh.fetchSearchedData(questionNo: searchText)
+           // sdata = dh.fetchSearchedData(questionNo: searchText)
+           sdata =  dh.fetchSearchedData(searchText: searchText)
             tableview.reloadData()
         }
-//        else if searchText != ""{
-//            sdata2 = dh.fetchSearchData2(id: searchText)
-//            tableview.reloadData()
-//
-//        }
-        else{
-            //print("try some better options ")
-            print("")
-        }
+        
     }
 }
