14450b976b2ef2528489ecbde90698432146f56c
“Mayank <azizsaifee123@gmail.com>
2023-03-01 13:19:15 +0530

    Introduced search bar in the new update - Ankit

---
+++ Project Management/View/Search/DataBaseHandler.swift
@@ -0,0 +1,50 @@
+//
+//  DataBaseHandler.swift
+//  Project Management
+//
+//  Created by 5Exceptions_Mac1 on 27/02/23.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class DataBaseHandler {
+    
+
+    func fetchSearchedData(questionNo:String) -> [Questions]
+    {
+
+        var sdata = [Questions]()
+let predicate = NSPredicate(format: "questionNo CONTAINS[cd] %@ OR question CONTAINS[cd] %@ OR option1 CONTAINS[cd] %@ OR option2 CONTAINS[cd] %@ OR option3 CONTAINS[cd] %@ OR option4 CONTAINS[cd] %@" , questionNo,questionNo,questionNo,questionNo,questionNo,questionNo)
+        let context = PersistentStorage.shared.context
+        let request:NSFetchRequest = Questions.fetchRequest()
+        request.predicate = predicate
+        do{
+            sdata = try(context.fetch(request))
+        }
+        catch{
+            print("error")
+        }
+        return sdata
+    }
+    
+//    func fetchSearchedData(searchText: String) -> [Questions] {
+//        var sdata = [Questions]()
+//        let context = PersistentStorage.shared.context
+//        let request: NSFetchRequest<Questions> = Questions.fetchRequest()
+//        let predicate = NSCompoundPredicate(orPredicateWithSubpredicates: [
+//            NSPredicate(format: "questionNo CONTAINS[c] %@", searchText),
+//            NSPredicate(format: "AppCoreData CONTAINS[c] %@", searchText),
+//            //NSPredicate(format: "UserDetail.property CONTAINS[c] %@", searchText)
+//        ])
+//        request.predicate = predicate
+//        do {
+//            sdata = try context.fetch(request)
+//        } catch {
+//            print("Error fetching searched data: \(error.localizedDescription)")
+//        }
+//        return sdata
+//    }
+
+}
---
+++ Project Management/View/Search/SearchTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  SearchTableViewCell.swift
+//  Project Management
+//
+//  Created by 5Exceptions_Mac1 on 27/02/23.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var questionlabel: UILabel!
+    
+    @IBOutlet weak var option1label: UILabel!
+    
+    @IBOutlet weak var option2label: UILabel!
+    
+    @IBOutlet weak var option3label: UILabel!
+    
+    @IBOutlet weak var option4label: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
